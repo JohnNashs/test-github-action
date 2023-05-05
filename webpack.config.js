badbc8ab7c24d12a5016eeaaaefc77429950658f
YuminFu <923410813@qq.com>
2023-05-05 20:15:12 +0800

    change webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: './index.js',
   plugins: [
     new HtmlWebpackPlugin({
       title: '测试CI/CD'
